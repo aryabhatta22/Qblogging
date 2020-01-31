b70e1a8e4b5fd7b129f5f59c18c60351969de021
aryabhatta <tarun.hive@hotmail.com>
2020-01-31 17:03:11 +0530

    modified tempdata

--- tempData/genratekeys.php
+++ tempData/genratekeys.php
@@ -13,3 +13,16 @@ $pubKey = $pubKey["key"];
 
 // Private key: $privKey
 // public key: $pubKey
+
+
+
+/*********Private and public key verification**********/
+
+$data = "Verified";
+
+  openssl_sign($data, $raw_signature, $privKey);
+  $signature = base64_encode($raw_signature);
+  $raw_signature = base64_decode($signature);
+  $result = openssl_verify($data, $raw_signature, $pubKey);
+  echo $result;
+  // returns 1 if data and signature match
