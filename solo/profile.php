6c9570573023512dd3e1a4875178a4fcd5c2391b
Sandeep <sk9958814616@gmail.com>
2020-02-01 14:39:16 +0530

    finalize final

--- solo/profile.php
+++ solo/profile.php
@@ -52,6 +52,40 @@ $departmentId=$_SESSION['dept_id'];
         .card-body{
             padding: 10px;
         }
+
+        .slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 15px;
+  border-radius: 5px;
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: black;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: black;
+  cursor: pointer;
+}
     </style>
 </head>
 <body>
@@ -98,28 +132,123 @@ $departmentId=$_SESSION['dept_id'];
     </div>
   </div>
   <div class="col-sm-8">
-    <div class="card">
-      <div class="card-body">
-        
-        <div class="card-body" style="border-color:beige; background-color: #f5f5f0;">
-      <div class="row">
-  <div class="col-sm-2"><img src=".\images\a1.jpg" class="img-thumbnail" alt="" width="50" height="50"></div>
-  <div class="col-sm-6">User Id</div>
-</div>
-      <hr>
-    <blockquote class="blockquote mb-0">
-      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</h3>
-        jhwsdshdjskdeskfesfjesfrkfrdfr<br>
-        sedehdhefefejfeifdiej<br>
-        sdgasdhhdjwdjwj
-      <footer class="blockquote-footer"> <cite title="Source Title">author</cite>
-        <br>
-        <cite>10:00:00</cite></footer>
-    </blockquote>
-  </div>
-      </div>
-    </div>
-  </div>
+                  <div class="card" id="card">
+                        
+                
+                  <?php
+                    //run a query to look for notes corresponding to user_id
+                        $sql = "SELECT * FROM post WHERE status='1' ORDER BY time DESC LIMIT 5";
+
+                        //shows notes or alert message
+                        if($result = mysqli_query($link, $sql)){
+                            if(mysqli_num_rows($result)>0){
+                                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                                    $user_id = $row['user_id'];
+                                    $title = $row['title'];
+                                    $data=$row['data'];
+                                    $time = $row['time'];
+                                    $time = date("F d, Y h:i:s A", $time);
+                                    $sql2="SELECT * FROM user WHERE user_id='$user_id'";
+                                    $result2 = mysqli_query($link, $sql2);
+                                    if(!$result2){
+                                          echo '<div class="alert alert-danger">Error running the query!</div>';
+                                          exit;
+                                      }
+                                      $row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+
+                                      $currentpostusername=$row2['name'];
+                                      $currentpostusertype=$row2['profession'];
+
+                                    $type=$_SESSION['profession'];
+                                    if($type=="student"){
+                                        echo "
+                                      <div class='card-body'>
+                                          <div class='card-body' style='border-color:beige; background-color: #f5f5f0;'>
+                                              <div class='row'>
+                                                  <div class='col-sm-2'><img src='.\images\a1.jpg' class='img-thumbnail'  width='50' height='50'></div>
+                                                  <div class='col-sm-6'>$user_id</div>
+                                              </div>
+                                            <hr>
+                                          <blockquote class='blockquote mb-0'>
+                                            <h3>$title.</h3>
+                                              <p>$data</p>
+                                            <footer class='blockquote-footer'> <cite title='Source Title'>$currentpostusername</cite>
+                                              <br>
+                                              <cite>$time</cite></footer>
+                                              <input class='btn btn-info pull-right' type='submit' name='upvote' value='Up vote' id='upvote'>
+                                              <br>
+                                          </blockquote>
+                                          </div>
+                                      </div>
+                                  ";
+                                    }else{
+
+                                        if($currentpostusertype == "student"){
+                                             echo "
+                                            <div class='card-body'>
+                                                <div class='card-body' style='border-color:beige; background-color: #f5f5f0;'>
+                                                    <div class='row'>
+                                                        <div class='col-sm-2'><img src='.\images\a1.jpg' class='img-thumbnail'  width='50' height='50'></div>
+                                                        <div class='col-sm-6'>$user_id</div>
+                                                    </div>
+                                                  <hr>
+                                                <blockquote class='blockquote mb-0'>
+                                                  <h3>$title.</h3>
+                                                    <p>$data</p>
+                                                  <footer class='blockquote-footer'> <cite title='Source Title'>$currentpostusername</cite>
+                                                    <br>
+                                                    <cite>$time</cite></footer>
+                                                    <br>
+                                                    <div class='slidecontainer'>
+                                                      <input class='pull-right' type='range' min='1' max='10' value='5' class='slider' id='myRange' style='width:20%;''>
+                                                      <br>
+                                                      <p class='pull-right'>Rating... <span id='demo'></span></p>
+                                                    </div>
+                                                    <br>
+                                                </blockquote>
+                                                </div>
+                                            </div>
+                                        ";
+                                        }else{
+                                            echo "
+                                                <div class='card-body'>
+                                                    <div class='card-body' style='border-color:beige; background-color: #f5f5f0;'>
+                                                        <div class='row'>
+                                                            <div class='col-sm-2'><img src='.\images\a1.jpg' class='img-thumbnail'  width='50' height='50'></div>
+                                                            <div class='col-sm-6'>$user_id</div>
+                                                        </div>
+                                                      <hr>
+                                                    <blockquote class='blockquote mb-0'>
+                                                      <h3>$title.</h3>
+                                                        <p>$data</p>
+                                                      <footer class='blockquote-footer'> <cite title='Source Title'>$currentpostusername</cite>
+                                                        <br>
+                                                        <cite>$time</cite></footer>
+                                                        <input class='btn btn-info pull-right' type='submit' name='upvote' value='Up vote' id='upvote'>
+                                                        <br>
+                                                    </blockquote>
+                                                    </div>
+                                                </div>
+                                            ";
+
+                                        }
+                                     
+                                    }
+                                          
+                                    
+                                }
+                            }else{
+                                echo '<div class="alert alert-warning">You have not created any notes yet!</div>'; exit;
+                            }
+                            
+                        }else{
+                            echo '<div class="alert alert-warning">An error occured!</div>'; exit;
+                        //    echo "ERROR: Unable to excecute: $sql." . mysqli_error($link);
+                        }
+
+                  ?>
+                </div>
+            </div>
   <div class="col-sm-1" style="margin-left:0px; margin-right:-50px;">
     <div class="card" style="width:20em;">
       <div class="card-body">
@@ -146,6 +275,16 @@ $departmentId=$_SESSION['dept_id'];
 </div>
         </div>
     </div>
+
+    <script>
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+      output.innerHTML = this.value;
+    }
+    </script>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
